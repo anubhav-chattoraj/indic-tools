bbc66f863c78e1be8826558ade2fb729cf85c0a6
Anubhav Chattoraj <anubhav.chattoraj@gmail.com>
2013-10-25 19:24:38 +0530

    Implement options for customizing sort order

--- js/sort_devanagari.js
+++ js/sort_devanagari.js
@@ -1,6 +1,10 @@
+function debug(word) {
+  console.log(word.primary + '    ' + word.secondary);
+}
+
 var sort_devanagari = (function($){
+  "use strict";
   return function(words, options){
-    "use strict";
 
     var weights = new Hashtable(), matras = new Hashtable(), consonants = new HashSet();
 
@@ -16,7 +20,7 @@ var sort_devanagari = (function($){
       for(i = 0; i < order.length; i++) {
         // The multiples of 10 make it easy to insert more letters in between
         weights.put(order.charAt(i), {primary: (i+1)*10, secondary: 0});
-      };
+      }
       for(i = 0; i < s_lowercase.length; i++) {
         this_char = s_lowercase.charAt(i);
         weights.put(this_char, {
@@ -45,8 +49,8 @@ var sort_devanagari = (function($){
       }
     })();
 
-    var processed_words = $.map(words, function(word, idx) {
-      var processed_word = {idx: idx, primary: [], secondary: []},
+    function process_word(word) {
+      var processed_word = {primary: [], secondary: []},
         possible_schwa = false, i, this_char;
 
       function push_weight(letter) {
@@ -87,9 +91,85 @@ var sort_devanagari = (function($){
       if(options.rtl) {
         processed_word.primary.reverse(); processed_word.secondary.reverse();
       }
+      console.log(word); debug(processed_word);
+      return processed_word;
+    }
 
+    var processed_words = $.map(words, function(word, idx) {
+      var processed_word = process_word(word);
+      processed_word.idx = idx;
       return processed_word;
     });
+
+    var replacements = new Hashtable();
+    (function construct_replacements(){
+      if(options.change_anusvara) {
+        var vargas = [ 'कखगघङ', 'चछजझञ', 'टठडढण', 'तथदधन', 'पफबभम'],
+          anusvara_weight = weights.get('ं');
+        $.each(vargas, function(idx, varga) {
+          var i, char_weight, nasal_weight = weights.get(varga.charAt(varga.length - 1));
+          for(i = 0; i < varga.length; i++) {
+            char_weight = weights.get(varga.charAt(i));
+            replacements.put([anusvara_weight, char_weight], [nasal_weight, char_weight]);
+          }
+        });
+      }
+      if(options.change_visarga) {
+        console.log('here');
+        var i, sibilants = 'शषस', visarga_weight = weights.get('ः'), char_weight;
+        for(i = 0; i < sibilants.length; i++) {
+          char_weight = weights.get(sibilants.charAt(i));
+          replacements.put([visarga_weight, char_weight], [char_weight, char_weight]);
+        }
+      }
+      if(options.separate_ksa) {
+        var ksa_weight = {primary: weights.get('ळ').primary + 3, secondary: 0};
+        weights.put('क्ष', ksa_weight);
+        replacements.put([weights.get('क'), weights.get('ष')], [ ksa_weight ]);
+      }
+      if(options.separate_tra) {
+        var tra_weight = {primary: weights.get('ळ').primary + 4, secondary: 0};
+        weights.put('त्र', tra_weight);
+        replacements.put([weights.get('त'), weights.get('र')], [ tra_weight ]);
+      }
+      if(options.separate_jna) {
+        var jna_weight = {primary: weights.get('ळ').primary + 5, secondary: 0};
+        weights.put('ज्ञ', jna_weight);
+        replacements.put([weights.get('ज'), weights.get('ञ')], [ jna_weight ]);
+      }
+    })();
+
+    (function perform_replacement() {
+      $.each(replacements.keys(), function(idx, lhs){
+        var rhs = replacements.get(lhs); 
+        $.each(processed_words, function(word_idx, word){
+          var i, j, match, tmp_primary, tmp_secondary;
+          for(i = 0; i < word.primary.length - lhs.length; i++) {
+            match = true;
+            for(j = 0; j < lhs.length; j++) {
+              if(word.primary[i+j] !== lhs[j].primary || word.secondary[i+j] !== lhs[j].secondary) {
+                match = false;
+                break;
+              }
+            }
+
+            if(match) {
+              console.log('before: '+word.primary);
+              tmp_primary = []; tmp_secondary = [];
+              for(j = 0; j < rhs.length; j++) {
+                tmp_primary.push(rhs[j].primary);
+                tmp_secondary.push(rhs[j].secondary);
+              }
+              word.primary = word.primary.slice(0, i).concat(tmp_primary).
+                concat(word.primary.slice(i+lhs.length, word.primary.length));
+              word.secondary = word.secondary.slice(0, i).concat(tmp_secondary).
+                concat(word.secondary.slice(i+lhs.length, word.secondary.length));
+              console.log('after:  '+word.primary);
+            }
+          }
+        }); 
+      });
+    })();
 
     function compare_processed_words(word1, word2) {
       var i;
@@ -115,7 +195,7 @@ var sort_devanagari = (function($){
       return 0;
     }
     processed_words.sort(compare_processed_words);
-
+    
     var result = $.map(processed_words, function(processed_word, idx) {
       return words[processed_word.idx];
     });
@@ -123,6 +203,10 @@ var sort_devanagari = (function($){
     if(options.descending) {
       result.reverse();
     }
+    
+    $.each(processed_words, function(idx, word){
+      console.log(word.primary);
+    });
     return result;
   };
 })(jQuery);
